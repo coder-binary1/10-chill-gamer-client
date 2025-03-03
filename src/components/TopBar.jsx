8cd346060fddd7e92ce404bd5cb394827b7d0c21
coder-binary1 <noman.g341@gmail.com>
2025-03-04 04:29:18 +0600

    tooltip feature added

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,6 +1,7 @@
 import { FaFacebookF, FaTwitter, FaTwitch } from "react-icons/fa";
 import { SlLogout } from "react-icons/sl";
 import { CgProfile } from "react-icons/cg";
+import { Tooltip } from "react-tooltip";
 import { Link, useNavigate } from "react-router-dom";
 import { useContext, useRef, useState } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
@@ -44,10 +45,14 @@ const TopBar = () => {
             onClick={toggleDropdown}
           >
             <img
+              data-tooltip-id="my-tooltip"
+              data-tooltip-content={user.displayName}
+              data-tooltip-place="left"
               className="w-8 rounded-full relative"
               src={user.photoURL}
               alt={user.displayName}
             />
+            <Tooltip id="my-tooltip" />
           </button>
           {isOpen && (
             <ul className="menu dropdown-content bg-base-100 rounded-box z-1 p-2 shadow-sm">
